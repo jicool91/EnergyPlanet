bcd5628cae833ce645b452ffd389bc6207a27e31
Дмитрий Февралев <jicool@MacBook-Pro-Dmitrij.local>
2025-10-30 16:58:21 +0300

    refactor: inline prometheus config rendering

--- infra/prometheus/entrypoint.sh
+++ infra/prometheus/entrypoint.sh
@@ -1,10 +1,34 @@
 #!/bin/sh
 set -eu
 
-/bin/envsubst "${PROMETHEUS_SCRAPE_TARGET} ${PROMETHEUS_SCRAPE_SCHEME} ${PROMETHEUS_METRICS_PATH} ${PROM_AUTH_USER} ${PROM_AUTH_PASS}" \
-  < /etc/prometheus/prometheus.yml.template > /etc/prometheus/prometheus.yml
+cat <<EOF >/etc/prometheus/prometheus.yml
+global:
+  scrape_interval: 15s
+  evaluation_interval: 15s
 
-exec /bin/prometheus \
-  --config.file=/etc/prometheus/prometheus.yml \
-  --web.enable-lifecycle \
-  --web.listen-address='[::]:9090'
+scrape_configs:
+  - job_name: 'energyplanet-api'
+    honor_labels: true
+    honor_timestamps: true
+    scheme: ${PROMETHEUS_SCRAPE_SCHEME}
+    metrics_path: ${PROMETHEUS_METRICS_PATH}
+    static_configs:
+      - targets:
+          - ${PROMETHEUS_SCRAPE_TARGET}
+EOF
+
+if [ "${PROM_AUTH_USER:-}" != "" ] && [ "${PROM_AUTH_PASS:-}" != "" ]; then
+cat <<EOF >>/etc/prometheus/prometheus.yml
+    basic_auth:
+      username: ${PROM_AUTH_USER}
+      password: ${PROM_AUTH_PASS}
+EOF
+fi
+
+cat <<'EOF' >>/etc/prometheus/prometheus.yml
+    relabel_configs:
+      - source_labels: [__address__]
+        target_label: instance
+EOF
+
+exec /bin/prometheus   --config.file=/etc/prometheus/prometheus.yml   --web.enable-lifecycle   --web.listen-address='[::]:9090'
