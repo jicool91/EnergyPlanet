cce44beeef8796be5cfd190870490c5a5f4b9ab0
Дмитрий Февралев <jicool@MacBook-Pro-Dmitrij.local>
2025-11-08 18:52:26 +0300

    chore: declare shared token module

---
+++ webapp/src/types/shared.d.ts
@@ -0,0 +1,9 @@
+declare module '@shared/tokens/safe-area.json' {
+  const value: {
+    headerReservePx: number;
+    headerBufferPx: number;
+    navigationReservePx: number;
+    sidePaddingPx: number;
+  };
+  export default value;
+}
